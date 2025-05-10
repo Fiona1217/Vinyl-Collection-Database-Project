d002aad2dec2470ce8d53fce63e7f70b88bce238
HJL <167211523+Fiona1217@users.noreply.github.com>
2025-05-09 20:27:18 -0400

    Create cleanup.sql

---
+++ cleanup.sql
@@ -0,0 +1,10 @@
+-- Connect as SYSDBA
+CONNECT sys AS sysdba;
+
+DROP PROCEDURE groupAssignment2_admin.sp_checkInvalidArtistName;
+-- Drop the user and tablespace
+DROP USER groupAssignment2_admin CASCADE;
+DROP TABLESPACE cst2355_GroupAssignment2 INCLUDING CONTENTS AND DATAFILES;
+
+COMMIT;
+-- End of File
